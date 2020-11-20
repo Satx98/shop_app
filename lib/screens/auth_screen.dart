4b280bf215571567cba3b3893316a01bcd75f5d8
Satadru Roy <satadru@Satadrus-MacBook-Air.local>
2020-11-20 22:45:43 +0530

    Used AnimatedBuilder instead of .addListener()

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -122,17 +122,17 @@ class _AuthCardState extends State<AuthCard>
       ),
     );
 
-    _heightAnimation.addListener(() {
-      setState(() {});
-    });
+    // _heightAnimation.addListener(() {
+    //   setState(() {});
+    // });
   }
 
   @override
   void dispose() {
     super.dispose();
-    _heightAnimation.removeListener(() {
-      setState(() {});
-    });
+    // _heightAnimation.removeListener(() {
+    //   setState(() {});
+    // });
     _controller.dispose();
   }
 
@@ -228,15 +228,19 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.SignUp ? 320 : 260,
-        height: _heightAnimation.value.height,
-        constraints: BoxConstraints(
-          // minHeight: _authMode == AuthMode.SignUp ? 320 : 260,
-          minHeight: _heightAnimation.value.height,
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+          // height: _authMode == AuthMode.SignUp ? 320 : 260,
+          height: _heightAnimation.value.height,
+          constraints: BoxConstraints(
+            // minHeight: _authMode == AuthMode.SignUp ? 320 : 260,
+            minHeight: _heightAnimation.value.height,
+          ),
+          width: deviceSize.width * 0.75,
+          padding: const EdgeInsets.all(16.0),
+          child: ch,
         ),
-        width: deviceSize.width * 0.75,
-        padding: const EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
